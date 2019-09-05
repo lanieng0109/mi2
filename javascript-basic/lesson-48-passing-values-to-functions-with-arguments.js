d3d282cfafbb5c3be85e88ce969815361a30987f
Lana <lan.ngohuong@gmail.com>
2019-09-05 12:15:46 +0300

    Added 48th lesson

---
+++ javascript-basic/lesson-48-passing-values-to-functions-with-arguments.js
@@ -0,0 +1,13 @@
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(param1, param2) {
+  console.log(param1 + param2);
+}
+
+functionWithArgs (1,2);
